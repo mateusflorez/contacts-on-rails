329260da6f1ae6d75c90f48c797ba0f49f257807
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-08-15 14:28:43 -0300

    form and validation in user sign up

--- Gemfile
+++ Gemfile
@@ -30,3 +30,5 @@ group :test do
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "bcrypt", "~> 3.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :contacts
   root to: "static_pages#index"
+
   get 'about', to: 'static_pages#about'
   get 'elsewhere', to: 'static_pages#contact'
+
+  resources :contacts
+  resources :users, only: [:new, :create]
 end

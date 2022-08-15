32556555bd466ba1fdc1a858adcbb34bf0ba687f
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-08-15 16:28:03 -0300

    login functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
   root to: "static_pages#index"
 
   get 'about', to: 'static_pages#about'
   get 'elsewhere', to: 'static_pages#contact'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :contacts
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 end
